658d2959c77f3199db749ed18af4ea1bb2b39143
svelout <64208856+svelout@users.noreply.github.com>
2023-03-02 21:43:27 +0300

    Fix

--- SpawnCubesController.cs
+++ SpawnCubesController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Timers;
 using TMPro;
+using UnityEditor;
 
 #nullable disable
 public class SpawnCubes : MonoBehaviour
@@ -29,11 +30,11 @@ public class SpawnCubes : MonoBehaviour
     public bool TimerStarted;
     public bool IsRecordAdd;
     public bool record_write;
+    public int pre_ran;
     public int current_sec;
-    public int pre_rand;
-    public int cur_rand;
     public int record = 0;
     public int global_record;
+    public int cur_rand;
     public TextMeshProUGUI Timer;
     public TextMeshProUGUI RecordText;
     public TextMeshProUGUI NewRecordText;   
@@ -43,7 +44,7 @@ public class SpawnCubes : MonoBehaviour
 
     void Start()
     {
-        pre_rand = 0;
+        pre_ran = 0;
         record_write = false;
         IsRecordAdd = false;
         current_sec = 3;
@@ -71,120 +72,117 @@ public class SpawnCubes : MonoBehaviour
     void Update()
     {
         cur_rand = Random.Range(1, 10);
-        if (cur_rand != pre_rand)
+        if (GameValues.End == true && record_write == false)
         {
-            if (GameValues.End == true && record_write == false)
+            if (global_record > GameValues.global_record)
             {
-                if (global_record > GameValues.global_record)
+                GameValues.global_record = global_record;
+                SaveRecord.WriteValueRecordFile(GameValues.global_record, GameValues.record_file);
+                NewRecordText.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -0.16f);
+                record_write = true;
+            }
+        }
+        if (GameValues.restart == true)
+        {
+            foreach (GameObject obj in objects)
+            {
+                Destroy(obj.gameObject);
+            }
+            objects.Clear();
+            foreach (GameObject obj in GameController.minicubes)
+            {
+                Destroy(obj.gameObject);
+            }
+            GameController.minicubes.Clear();
+            GameValues.End = false;
+            GameValues.restart = false;
+        }
+        if (current_sec != 0)
+        {
+            Timer.text = current_sec.ToString();
+        }
+        if (GameValues.End != true && GameValues.Start != false && cur_rand != pre_ran)
+        {
+            if (Timer.fontMaterial.GetFloat(ShaderUtilities.ID_FaceDilate) != -1f)
+            {
+                Timer.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -1f);
+            }
+            if (RecordText.fontMaterial.GetFloat(ShaderUtilities.ID_FaceDilate) == -1f)
+            {
+                RecordText.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -0.2f);
+            }
+            if (NewRecordText.fontMaterial.GetFloat(ShaderUtilities.ID_FaceDilate) != -1f)
+            {
+                NewRecordText.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -1f);
+            }
+            if (GameValues.GameMusic != false && gm.isPlaying != true)
+            {
+                gm.Play();
+            }
+            timer.Enabled = false;
+            pre_ran = cur_rand;
+            int count = 1;
+            if (isgenerated != true)
+            {
+                for (float x = _platform_x; x >= platform_x; x -= add_value)
                 {
-                    GameValues.global_record = global_record;
-                    SaveRecord.WriteValueRecordFile(GameValues.global_record, GameValues.record_file);
-                    NewRecordText.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -0.16f);
-                    record_write = true;
+                    if (count == cur_rand)
+                    {
+                        count++;
+                        var s_obj = Instantiate(CluePlane, new Vector3(x - (add_value / 2) + 0.095f, clplane_y, clplane_z), Quaternion.identity);
+                        var e_obj = Instantiate(CluePlane, new Vector3((x + (add_value / 2)) - 0.058f, clplane_y, clplane_z), Quaternion.identity);
+                        clue.Add(s_obj);
+                        clue.Add(e_obj);
+                    }
+                    else
+                    {
+                        var obj = Instantiate(ScriptCube, new Vector3(x, transform.position.y + (1f - 0.4f), Platform.transform.position.z + (21.53f - 7.13f)), Quaternion.identity);
+                        objects.Add(obj);
+                        count++;
+                    }
+                }
+                isgenerated = true;
+            }
+            foreach (GameObject obj in objects)
+            {
+                if (GameValues.End == true) break;
+                if (obj.transform.position.z >= platform_z)
+                {
+                    objects_pos = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
+                    objects_pos.z -= add_value * (Time.deltaTime * speed);
+                    obj.transform.position = objects_pos;
+                }
+                if (obj.transform.position.z <= add_record_pos && IsRecordAdd != true)
+                {
+                    foreach (GameObject clue in this.clue)
+                    {
+                        Destroy(clue);
+                    }
+                    record++;
+                    global_record = record;
+                    RecordText.text = $"Score: {record}";
+                    IsRecordAdd = true;
+                    clue.Clear();
+                }
+                if (obj.transform.position.z <= platform_z)
+                {
+                    IsEnd = true;
+                    break;
                 }
             }
-            if (GameValues.restart == true)
+            if (IsEnd == true)
             {
+                isgenerated = false;
                 foreach (GameObject obj in objects)
                 {
                     Destroy(obj.gameObject);
                 }
                 objects.Clear();
-                foreach (GameObject obj in GameController.minicubes)
-                {
-                    Destroy(obj.gameObject);
-                }
-                GameController.minicubes.Clear();
-                GameValues.End = false;
-                GameValues.restart = false;
+                isgenerated = false;
+                IsEnd = false;
+                IsRecordAdd = false;
             }
-            if (current_sec != 0)
-            {
-                Timer.text = current_sec.ToString();
-            }
-            if (GameValues.End != true && GameValues.Start != false)
-            {
-                if (Timer.fontMaterial.GetFloat(ShaderUtilities.ID_FaceDilate) != -1f)
-                {
-                    Timer.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -1f);
-                }
-                if (RecordText.fontMaterial.GetFloat(ShaderUtilities.ID_FaceDilate) == -1f)
-                {
-                    RecordText.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -0.2f);
-                }
-                if (NewRecordText.fontMaterial.GetFloat(ShaderUtilities.ID_FaceDilate) != -1f)
-                {
-                    NewRecordText.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -1f);
-                }
-                if (GameValues.GameMusic != false && gm.isPlaying != true)
-                {
-                    gm.Play();
-                }
-                timer.Enabled = false;
-                pre_rand = cur_rand;
-                int count = 1;
-                if (isgenerated != true)
-                {
-                    for (float x = _platform_x; x >= platform_x; x -= add_value)
-                    {
-                        if (count == cur_rand)
-                        {
-                            count++;
-                            var s_obj = Instantiate(CluePlane, new Vector3(x - (add_value / 2) + 0.095f, clplane_y, clplane_z), Quaternion.identity);
-                            var e_obj = Instantiate(CluePlane, new Vector3((x + (add_value / 2)) - 0.058f, clplane_y, clplane_z), Quaternion.identity);
-                            clue.Add(s_obj);
-                            clue.Add(e_obj);
-                        }
-                        else
-                        {
-                            var obj = Instantiate(ScriptCube, new Vector3(x, transform.position.y + (1f - 0.4f), Platform.transform.position.z + (21.53f - 7.13f)), Quaternion.identity);
-                            objects.Add(obj);
-                            count++;
-                        }
-                    }
-                    isgenerated = true;
-                }
-                foreach (GameObject obj in objects)
-                {
-                    if (GameValues.End == true) break;
-                    if (obj.transform.position.z >= platform_z)
-                    {
-                        objects_pos = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
-                        objects_pos.z -= add_value * (Time.deltaTime * speed);
-                        obj.transform.position = objects_pos;
-                    }
-                    if (obj.transform.position.z <= add_record_pos && IsRecordAdd != true)
-                    {
-                        foreach (GameObject clue in this.clue)
-                        {
-                            Destroy(clue);
-                        }
-                        record++;
-                        global_record = record;
-                        RecordText.text = $"Score: {record}";
-                        IsRecordAdd = true;
-                        clue.Clear();
-                    }
-                    if (obj.transform.position.z <= platform_z)
-                    {
-                        IsEnd = true;
-                        break;
-                    }
-                }
-                if (IsEnd == true)
-                {
-                    isgenerated = false;
-                    foreach (GameObject obj in objects)
-                    {
-                        Destroy(obj.gameObject);
-                    }
-                    objects.Clear();
-                    isgenerated = false;
-                    IsEnd = false;
-                    IsRecordAdd = false;
-                }
-            }
-        }        
+        }
     }
     private void OnTimerTick(object sender, ElapsedEventArgs e)
     {

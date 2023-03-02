979d56c6fb8940c69612c1a8db687038996f032f
svelout <64208856+svelout@users.noreply.github.com>
2023-03-02 18:24:57 +0300

    Alpha Ver
    Verison 0.1.0

---
+++ SknsMenuLoad.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SknsMenuLoad : MonoBehaviour
+{
+    public AudioSource ms;
+    void Start()
+    {
+        ms.Play();
+    }
+}
